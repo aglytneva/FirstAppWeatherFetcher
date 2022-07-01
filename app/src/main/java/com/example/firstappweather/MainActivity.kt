7a560400d38258bc92db18122296648a80eed4f6
aglytneva <lytneva.a@yandex.ru>
2022-07-02 02:13:47 +0300

    - Initial commit 2

---
+++ app/src/main/java/com/example/firstappweather/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.firstappweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textViewHello = findViewById<TextView>(R.id.tvHello)
+        textViewHello.text = "Привет, мое первое приложение!"
+    }
+}
